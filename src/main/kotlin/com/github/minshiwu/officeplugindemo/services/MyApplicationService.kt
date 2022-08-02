83c06fbdf29a70c07f1ee143b3babcd2b7f72307
GitHub Action <action@github.com>
2022-08-02 11:09:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/minshiwu/officeplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/minshiwu/officeplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.minshiwu.officeplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.minshiwu.officeplugindemo.MyBundle
 
 class MyApplicationService {
 
